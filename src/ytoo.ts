c97ece3e9dc10b73c8debc8e29435ad98c32b0c6
megrez <lujiajing1126@gmail.com>
2019-10-09 19:37:56 +0800

    fix ytoo sg

--- src/ytoo.ts
+++ src/ytoo.ts
@@ -2,7 +2,7 @@ import Resolver from "./resolver";
 import { ServerBuilder } from "./server";
 
 const YTOOInboundsMap: {[key: string]: string} = { 穗: "GZ", 沪: "SH", 京: "BJ"};
-const YTOOOutboundsMap: {[key: string]: string} = { 台: "TW", 德: "DE", 日: "JP", 美: "US", 港: "HK", 韩: "KR"};
+const YTOOOutboundsMap: {[key: string]: string} = { 台: "TW", 德: "DE", 日: "JP", 美: "US", 港: "HK", 韩: "KR", 新: "SG"};
 const YTOOServerTypes: string[] = ["KT", "GIA", "NTT", "OCN", "CHT", "IIJ"];
 
 class YTOOResolver extends Resolver {
