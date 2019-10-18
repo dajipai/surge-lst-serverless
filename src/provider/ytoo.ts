9d9ebc68f1375ae7ba410cac4d80234ad1d76189
megrez <lujiajing1126@gmail.com>
2019-10-18 17:43:28 +0200

    update ytoo inbound

--- src/provider/ytoo.ts
+++ src/provider/ytoo.ts
@@ -1,7 +1,7 @@
 import Resolver from "../resolver";
 import { ServerBuilder } from "../server";
 
-const YTOOInboundsMap: {[key: string]: string} = { 穗: "GZ", 沪: "SH", 京: "BJ"};
+const YTOOInboundsMap: {[key: string]: string} = { 穗: "GZ", 沪: "SH", 京: "BJ", 筑: "GY"};
 const YTOOOutboundsMap: {[key: string]: string} = { 台: "TW", 德: "DE", 日: "JP", 美: "US", 港: "HK", 韩: "KR", 新: "SG"};
 const YTOOServerTypes: string[] = ["KT", "GIA", "NTT", "OCN", "CHT", "IIJ"];
 
