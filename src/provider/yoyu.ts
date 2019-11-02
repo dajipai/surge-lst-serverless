9f107f898250e01edb345ec9610817c5fa5068ec
megrez <2568208+lujiajing1126@users.noreply.github.com>
2019-11-02 22:27:13 +0100

    provide IPLC as tag for YoYu

--- src/provider/yoyu.ts
+++ src/provider/yoyu.ts
@@ -3,7 +3,7 @@ import { ServerBuilder } from "../server";
 
 const YoYuInboundsMap: {[key: string]: string} = { 青岛: "QD", 上海: "SH", 沪: "SH", 北京: "BJ", 杭州: "HZ", 深圳: "SZ", 无锡: "WX", 呼: "HHHT"};
 const YoYuOutboundsMap: {[key: string]: string} = { 台湾: "TW", 德国: "DE", 日本: "JP", 狮城: "SG", 美国: "US", 香港: "HK", 港: "HK", 韩国: "KR", 伦敦: "UK", 东京: "JP", 首尔: "KR", 土耳其: "TR"};
-const YoYuServerTypes: string[] = ["BGP", "中继", "高级中继", "IPLC", "测试", "专线", "日用", "购物"];
+const YoYuServerTypes: string[] = ["BGP", "测试", "专线", "日用", "购物", "游戏"];
 
 class YoYuResolver extends Resolver {
     constructor() {
@@ -15,6 +15,11 @@ class YoYuResolver extends Resolver {
         const matches = builder.name.match(/\[(\d\.\d)\]/);
         if (matches != null) {
             builder.multiplier = matches[1];
+        }
+
+        // Only provide one tag `IPLC`
+        if (builder.name.includes("IPLC")) {
+            builder.tag = "IPLC";
         }
     }
 }
