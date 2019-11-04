3879307edfd7bafe292db5fe045f3388a9aedb94
megrez <2568208+lujiajing1126@users.noreply.github.com>
2019-11-04 23:31:12 +0100

    add userinfo header for ytoo

--- src/handler.ts
+++ src/handler.ts
@@ -90,10 +90,10 @@ export const ytoo: Handler<
       return Err(ValidationError.create(400, "emoji is not type of boolean"));
     }
     const context = new ProxyContext(new Subscription(), output);
-    const result = await context.handle(`https://ytoo.dev/modules/servers/V2raySocks/osubscribe.php?sid=${id}&token=${token}`, multiValueQueryStringParameters, ytooResolver, useEmoji === "true", sortMethod);
+    const result = await context.handle(`https://ytoo.xyz/modules/servers/V2raySocks/osubscribe.php?sid=${id}&token=${token}`, multiValueQueryStringParameters, ytooResolver, useEmoji === "true", sortMethod);
     return Ok({
       statusCode: 200,
-      headers: {"content-type": "text/plain"},
+      headers: {"content-type": "text/plain", ...context.respHeader},
       body: result
     });
   });
