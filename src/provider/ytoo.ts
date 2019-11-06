2f37f8474c459eb37eeecd204a1724c636173e29
megrez <2568208+lujiajing1126@users.noreply.github.com>
2019-11-06 09:52:24 +0100

    refactor ytoo provider

--- src/provider/ytoo.ts
+++ src/provider/ytoo.ts
@@ -3,7 +3,7 @@ import { ServerBuilder } from "../server";
 
 const YTOOInboundsMap: {[key: string]: string} = { 穗: "GZ", 沪: "SH", 京: "BJ", 筑: "GY"};
 const YTOOOutboundsMap: {[key: string]: string} = { 台: "TW", 德: "DE", 日: "JP", 美: "US", 港: "HK", 韩: "KR", 新: "SG"};
-const YTOOServerTypes: string[] = ["KT", "GIA", "NTT", "OCN", "CHT", "IIJ"];
+const YTOOServerTypes: string[] = [];
 
 class YTOOResolver extends Resolver {
     constructor() {
@@ -15,6 +15,17 @@ class YTOOResolver extends Resolver {
         const matches = builder.name.match(/\((\d\.\d)\)/);
         if (matches != null) {
             builder.multiplier = matches[1];
+        }
+
+        const tagMatcher = builder.name.match(/^([A-Za-z0-9]+)-/);
+        if (tagMatcher != null) {
+            builder.tag = tagMatcher[1];
+        }
+
+        const serverTypeMatcher = builder.name.match(/-(\w+)-/);
+        if (serverTypeMatcher != null) {
+            builder.serverType = serverTypeMatcher[1];
+            builder.tag = builder.serverType;
         }
     }
 }
