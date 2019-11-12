b2a796758449a3ec285b4d27d7ca4a5312f2450f
megrez <2568208+lujiajing1126@users.noreply.github.com>
2019-11-12 17:24:35 +0800

    update provider

--- src/provider/yoyu.ts
+++ src/provider/yoyu.ts
@@ -1,8 +1,8 @@
 import Resolver from "../resolver";
 import { ServerBuilder } from "../server";
 
-const YoYuInboundsMap: {[key: string]: string} = { 青岛: "QD", 上海: "SH", 沪: "SH", 北京: "BJ", 杭州: "HZ", 深圳: "SZ", 无锡: "WX", 呼: "HHHT"};
-const YoYuOutboundsMap: {[key: string]: string} = { 台湾: "TW", 德国: "DE", 日本: "JP", 狮城: "SG", 美国: "US", 香港: "HK", 港: "HK", 韩国: "KR", 伦敦: "UK", 东京: "JP", 首尔: "KR", 土耳其: "TR", 印度: "IN"};
+const YoYuInboundsMap: {[key: string]: string} = { 青岛: "QD", 上海: "SH", 沪: "SH", 北京: "BJ", 杭州: "HZ", 深圳: "SZ", 无锡: "WX", 呼: "HHHT", 成都: "CD" };
+const YoYuOutboundsMap: {[key: string]: string} = { 台湾: "TW", 德国: "DE", 日本: "JP", 狮城: "SG", 美国: "US", 香港: "HK", 港: "HK", 韩国: "KR", 伦敦: "UK", 东京: "JP", 首尔: "KR", 土耳其: "TR", 印度: "IN" };
 const YoYuServerTypes: string[] = ["BGP", "测试", "专线", "日用", "购物", "游戏", "GIA"];
 
 class YoYuResolver extends Resolver {
