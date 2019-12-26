0ec5e172684a63a9002ead6ce7c9de41bc704e24
megrez <2568208+lujiajing1126@users.noreply.github.com>
2019-12-26 23:32:31 +0800

    fix ytoo new nodes and update dep

--- src/provider/ytoo.ts
+++ src/provider/ytoo.ts
@@ -1,8 +1,8 @@
 import Resolver from "../resolver";
 import { ServerBuilder } from "../server";
 
-const YTOOInboundsMap: {[key: string]: string} = { 穗: "GZ", 沪: "SH", 京: "BJ", 筑: "GY", 广州: "GZ", 江苏: "JS"};
-const YTOOOutboundsMap: {[key: string]: string} = { 台: "TW", 德: "DE", 日: "JP", 美: "US", 港: "HK", 韩: "KR", 新: "SG", 俄罗斯: "RU", 台湾: "TW", 日本: "JP", 香港: "HK", 菲律宾: "PH"};
+const YTOOInboundsMap: {[key: string]: string} = { 穗: "GZ", 沪: "SH", 京: "BJ", 筑: "GY", 广州: "GZ", 江苏: "JS", 武汉: "WH", 广西: "GX", 重庆: "CQ", 辽宁: "LN"};
+const YTOOOutboundsMap: {[key: string]: string} = { 台: "TW", 德: "DE", 日: "JP", 美: "US", 港: "HK", 韩: "KR", 新: "SG", 俄罗斯: "RU", 台湾: "TW", 日本: "JP", 香港: "HK", 菲律宾: "PH", 狮城: "SG"};
 const YTOOServerTypes: string[] = ['日用', 'BGP'];
 
 class YTOOResolver extends Resolver {
