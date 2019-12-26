7a2966080ba02ad7633414488ac71c47ec43527e
megrez <2568208+lujiajing1126@users.noreply.github.com>
2019-12-26 23:49:54 +0800

    add more server types for ytoo

--- src/provider/ytoo.ts
+++ src/provider/ytoo.ts
@@ -3,7 +3,7 @@ import { ServerBuilder } from "../server";
 
 const YTOOInboundsMap: {[key: string]: string} = { 穗: "GZ", 沪: "SH", 京: "BJ", 筑: "GY", 广州: "GZ", 江苏: "JS", 武汉: "WH", 广西: "GX", 重庆: "CQ", 辽宁: "LN"};
 const YTOOOutboundsMap: {[key: string]: string} = { 台: "TW", 德: "DE", 日: "JP", 美: "US", 港: "HK", 韩: "KR", 新: "SG", 俄罗斯: "RU", 台湾: "TW", 日本: "JP", 香港: "HK", 菲律宾: "PH", 狮城: "SG"};
-const YTOOServerTypes: string[] = ['日用', 'BGP'];
+const YTOOServerTypes: string[] = ['日用', 'BGP', 'Test', 'None'];
 
 class YTOOResolver extends Resolver {
     constructor() {
