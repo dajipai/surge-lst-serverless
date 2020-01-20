1192f0c8f9db13a9ed70ceba047e2a2f3ebb6b71
megrez <2568208+lujiajing1126@users.noreply.github.com>
2020-01-20 17:23:17 +0800

    support Japan

--- src/provider/ssrpass.ts
+++ src/provider/ssrpass.ts
@@ -1,7 +1,7 @@
 import Resolver from "../resolver";
 
 const SSRPassSSInboundsMap: {[key: string]: string} = {};
-const SSRPassSSOutboundsMap: {[key: string]: string} = { Taiwan: "TW", America: "US" };
+const SSRPassSSOutboundsMap: {[key: string]: string} = { Taiwan: "TW", America: "US", Japan: "JP", Hongkong: "HK" };
 const SSRPassSSServerTypes: string[] = [];
 
 class SSRPassSSResolver extends Resolver {
